43186d1a7494391eab08c974de213125273c3c91
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-01-03 17:54:23 +0530

    fixed sidebar bug

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,22 @@
 function Sidebar({ children, isOpen, onClickClose }) {
   return (
-    <div
-      className={`p-5 fixed top-0 right-0 h-full w-full shadow-lg md:w-[50%] lg:w-[35%] bg-white z-50 transition transform duration-300 ${
-        isOpen ? "translate-x-0" : "translate-x-full"
-      }`}
-    >
-      <button
-        onClick={onClickClose}
-        className="absolute right-4 top-4 p-2 text-black font-bold"
+    <div>
+      <div
+        className={`p-5 fixed top-0 right-0 h-full w-full shadow-lg md:w-[50%] lg:w-[35%] bg-white z-50 transition transform duration-300 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
       >
-        x
-      </button>
-      {children}
-      <div></div>
+        <button
+          onClick={onClickClose}
+          className="absolute right-4 top-4 p-2 text-black font-bold"
+        >
+          x
+        </button>
+        {children}
+      </div>
+      {isOpen && (
+        <div className="fixed left-0 top-0 z-20 h-full w-full bg-black opacity-50"></div>
+      )}
     </div>
   );
 }
