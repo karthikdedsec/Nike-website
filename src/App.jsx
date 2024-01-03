8164189953b6a09363951344e5edbdb8db8ef09f
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-01-03 16:33:53 +0530

    finish the new arrivals section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "./components/Nav";
+import NewArrivalSection from "./components/NewArrivalSection";
 import ShoeDetail from "./components/ShoeDetail";
+import { SHOE_LIST } from "./constant";
 
 function App() {
   return (
     <div className="animate-fadeIn p-7 xl:px-24">
       <Nav />
       <ShoeDetail />
+      <NewArrivalSection items={SHOE_LIST} />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+function Card({ item }) {
+  return (
+    <div
+      className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}
+    >
+      <div className="p-8">
+        <div className="text-2xl font-bold">{item.title}</div>
+        <div className="mt-10 font-semibold underline underline-offset-4">
+          SHOP NOW +
+        </div>
+      </div>
+      <img className="absolute top-5 left-[40%] h-40 w-56" src={item.src} />
+    </div>
+  );
+}
+export default Card;
